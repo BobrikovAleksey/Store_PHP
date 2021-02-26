d0eccb53f4ac07bf9dd035a8772f3fa4c2a76651
BobrikovAleksey <bobrikov.spb@ya.ru>
2021-02-26 09:50:06 +0300

    Add main controller   (#1)

---
+++ controllers/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Возвращает домашнюю страницу
+ */
+function default_action() {
+    echo render('home.php', [
+        'title' => 'Главная страница',
+        'styles' => '<link rel="stylesheet" type="text/css" href="/css/home.css?t=' . POSTFIX . '">',
+    ]);
+}
