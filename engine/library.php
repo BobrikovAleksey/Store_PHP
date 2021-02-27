6fa6d16f8b52acc3afa1a75a840200fdae6b54d7
BobrikovAleksey <bobrikov.spb@ya.ru>
2021-02-27 11:39:01 +0300

    Add library to engine   (#9)

---
+++ engine/library.php
@@ -0,0 +1,309 @@
+<?php
+/** @noinspection SqlNoDataSourceInspection, SqlResolve */
+
+session_start();
+define('ROOT_DIR', dirname(__DIR__));
+define('POSTFIX', microtime(true).rand());
+
+/** ***********************************************
+ *                D A T E B A S E                 *
+ ************************************************ */
+
+/**
+ * Возвращает соединение с базой данных
+ * @return mysqli
+ */
+function getDatabase(): mysqli {
+    static $database;
+
+    if (empty($database)) {
+        $database = mysqli_connect(
+            'localhost',
+            'root',
+            'root',
+            'php_shop',
+            '3366'
+        );
+    }
+
+    return $database;
+}
+
+/** ***********************************************
+ *                  C H E C K S                   *
+ ************************************************ */
+
+/**
+ * Проверяет наличие заказа
+ * @param int $id
+ * @return bool
+ */
+function hasOrder(int $id): bool {
+    $userId = getUserID();
+
+    $sql = <<<QUERY
+        SELECT COUNT(*) AS `count`
+        FROM `orders`
+        WHERE `id` = {$id} AND `id_user` = {$userId}
+QUERY;
+
+    $result = mysqli_query(getDatabase(), $sql);
+    $row = mysqli_fetch_assoc($result);
+    return (int)$row['count'] > 0;
+}
+
+/**
+ * Проверяет наличие товара
+ * @param int $id
+ * @return bool
+ */
+function hasProduct(int $id): bool {
+    $sql = <<<QUERY
+        SELECT COUNT(*) AS `count`
+        FROM `goods`
+        WHERE `id` = {$id}
+QUERY;
+
+    $result = mysqli_query(getDatabase(), $sql);
+    $row = mysqli_fetch_assoc($result);
+    return (int)$row['count'] > 0;
+}
+
+/**
+ * Проверяет произведен ли вход
+ * @return bool
+ */
+function isLogin(): bool {
+    if (empty($_SESSION['user']) or empty($_SESSION['user']['name']) or empty($_SESSION['user']['email'])) {
+        logout();
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * Проверяет наличие прав администратора
+ * @return bool
+ */
+function isAdmin(): bool {
+    return isLogin() and $_SESSION['admin'];
+}
+
+/** ***********************************************
+ *       D A T A   F R O M   S E S S I O N        *
+ ************************************************ */
+
+/**
+ * Возвращает идентификатор пользователя из сессии
+ * @return int|null
+ */
+function getUserId(): ?int {
+    static $id;
+    if (empty($_SESSION['user']) or empty($_SESSION['user']['email'])) {
+        $id = null;
+    } elseif (empty($id)) {
+        $sql = <<<QUERY
+            SELECT `id`
+            FROM `users`
+            WHERE `email` = '{$_SESSION['user']['email']}'
+QUERY;
+
+        $result = mysqli_query(getDatabase(), $sql);
+        $row = mysqli_fetch_assoc($result);
+        $id = (int)$row['id'];
+    }
+
+    return $id;
+}
+
+/** ***********************************************
+ *      D A T A   F R O M   G E T / P O S T       *
+ ************************************************ */
+
+/**
+ * Возвращает идентификатор заказа из get-запроса
+ * @return int|null
+ */
+function getOrderId(): ?int {
+    if (isset($_GET['id']) and is_numeric($_GET['id'])) {
+        $id = (int)$_GET['id'];
+        if (hasOrder($id)) return $id;
+    }
+
+    return null;
+}
+
+/**
+ * Возвращает идентификатор товара из get-запроса
+ * @return int|null
+ */
+function getProductId(): ?int {
+    if (isset($_GET['id']) and is_numeric($_GET['id'])) {
+        $id = (int)$_GET['id'];
+        if (hasProduct($id)) return $id;
+    }
+
+    return null;
+}
+
+/** ***********************************************
+ *                     A P P                      *
+ ************************************************ */
+
+/**
+ * Возвращает количество товаров в корзине
+ * @return int
+ */
+function countCart(): int {
+    if (empty($_SESSION['cart'])) return 0;
+
+    return count(array_keys($_SESSION['cart']));
+}
+
+/**
+ * Возвращает количество заказов пользователя
+ * @return int
+ */
+function countOrders(): int {
+    if ($userId = getUserID() < 0) return 0;
+
+    $sql = <<<QUERY
+        SELECT COUNT(*) AS count
+        FROM `orders`
+        WHERE `id_user` = { $userId } AND `status` NOT IN ('Отменен', 'Выполнен')
+QUERY;
+
+    $result = mysqli_query(getDatabase(), $sql);
+    $row = mysqli_fetch_assoc($result);
+    return (int)$row['count'];
+}
+
+/**
+ * Возвращает путь к контроллеру
+ * @param string $controller
+ * @return string
+ */
+function getControllerPath(string $controller): string {
+    return ROOT_DIR . "/controllers/{$controller}.php";
+}
+
+/**
+ * Возвращает относительный путь к макету
+ * @param string $layout
+ * @return string
+ */
+function getLayoutPath(string $layout): string {
+    return "/layouts/{$layout}";
+}
+
+/**
+ * Возвращает путь к шаблону
+ * @param string $template
+ * @return string
+ */
+function getViewPath(string $template): string {
+    return ROOT_DIR . "/views/{$template}";
+}
+
+/**
+ * Возвращает главное меню
+ * @return array
+ */
+function getMenu(): array {
+    $result[] = [ 'title' => 'Главная', 'link' => '/?p=home' ];
+    $result[] = [ 'title' => 'Товары', 'link' => '/?p=goods' ];
+    if (isLogin()) {
+        $orders = countOrders();
+        $orders = $orders > 0 ? " ({$orders})" : '';
+        $result[] = [ 'title' => "Заказы {$orders}",    'link' => '/?p=orders' ];
+    }
+    $cart = countCart();
+    $cart = $cart > 0 ? " ({$cart})" : '';
+    $result[] = [ 'title' => "Корзина {$cart}", 'link' => '/?p=cart' ];
+    if (isAdmin()) {
+        $result[] = [ 'title' => 'Работа с <br> товарами', 'link' => '/?p=goods&a=table' ];
+        $result[] = [ 'title' => 'Работа с <br> заказами', 'link' => '/?p=orders&a=table' ];
+    }
+    if (isLogin()) {
+        $result[] = [ 'title' => $_SESSION['user']['name'], 'link' => '/?p=account' ];
+        $result[] = [ 'title' => 'Выйти', 'link' => '/?p=account&a=logout' ];
+    } else {
+        $result[] = [ 'title' => 'Войти', 'link' => '/?p=account&a=login' ];
+    }
+
+    return $result;
+}
+
+/**
+ * Изменяет location в header
+ * @param string $location
+ */
+function redirect(string $location = ''): void {
+    if ($location !== '') {
+        header("location: {$location}");
+    } elseif (isset($_SERVER['HTTP_REFERER'])) {
+        header("location: {$_SERVER['HTTP_REFERER']}");
+    } else {
+        header('location: /');
+    }
+}
+
+/** ********************************************* */
+
+/**
+ * Подключает шаблон
+ * @param string $template
+ * @param array $params
+ * @return string
+ */
+function renderTemplate(string $template, array $params = []): string {
+    $view = getViewPath($template);
+    ob_start();
+    extract($params);
+    if (file_exists($view)) {
+        /** @noinspection PhpIncludeInspection */
+        include_once $view;
+    }
+    return ob_get_clean();
+}
+
+/**
+ * Выводит страницу
+ * @param string $template
+ * @param array $params
+ * @param string $layout
+ * @return string
+ */
+function render(string $template, array $params = [], string $layout = 'main.php'): string {
+    $content = renderTemplate($template, $params);
+    $title = 'Интернет-магазин';
+    if (!empty($params['title'])) $title = $params['title'];
+
+    return renderTemplate(getLayoutPath($layout), [
+        'title' => $title,
+        'content' => $content,
+        'menu' => getMenu(),
+    ]);
+}
+
+/**
+ * Запускает приложение
+ */
+function run(): void {
+    $controller = getControllerPath('index.php');
+    if (isset($_GET['p']) and is_file(getControllerPath($_GET['p']))) {
+        $controller = getControllerPath($_GET['p']);
+    }
+
+    if (file_exists($controller)) {
+        /** @noinspection PhpIncludeInspection */
+        include_once $controller;
+    }
+
+    $action = 'default_action';
+    if (isset($_GET['a']) and function_exists($actionFromGet = "{$_GET['a']}_action")) {
+        $action = $actionFromGet;
+    }
+    $action();
+}
