c84e047e6ce7099416692459c7d3f2e014251383
BobrikovAleksey <bobrikov.spb@ya.ru>
2021-02-28 22:05:45 +0300

    Add component: header   (#20)

---
+++ views/components/header/menu.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @var array $menu
+ * @var string $page
+ */
+?>
+
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <a class="navbar-brand" href="/">Unistore</a>
+
+            <a class="navbar-brand pull-right hidden-sm hidden-md hidden-lg" href="#open-cart">
+                <i class="ion-bag"></i> 7
+            </a>
+        </div>
+
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <?php foreach ($menu as $key => $item) :?>
+                    <li class="<?= $key === $page ? 'active' : '' ?>">
+                        <a href="<?= $menu['link'] ?>"><?= $menu['title'] ?></a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+
+        <div class="search hidden-xs" data-style="hidden">
+            <div class="input">
+                <button type="button"><i class="ion-ios-search"></i></button>
+
+                <input type="text" name="search" value="" placeholder="Введите что вы ищите..." />
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ views/components/header/top-links.php
@@ -0,0 +1,11 @@
+<div class="toplinks">
+    <a href="#registration" data-toggle="modal" data-target="#Modal-Registration">
+        <i class="ion-person"></i> Registration
+    </a>
+
+    <a href="#signin" data-toggle="modal" data-target="#Modal-SignIn"><i class="ion-unlocked"></i> Sign In</a>
+
+    <a href="/"><i class="ion-ios-heart"></i> Favorites</a>
+
+    <a href="tel:+88121234567" class="hidden-xs"><i class="ion-android-call"></i> 8 (812) 123-45-67</a>
+</div>
