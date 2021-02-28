625836fe1b3543375fb70630bccc1a49a27d5702
BobrikovAleksey <bobrikov.spb@ya.ru>
2021-02-28 22:23:34 +0300

    Add home page   (#22)

---
+++ views/home.php
@@ -0,0 +1,22 @@
+<?php
+/** @noinspection PhpIncludeInspection */
+
+include_once getComponentPath('home/header')
+?>
+
+<hr class="offset-lg">
+<hr class="offset-lg">
+
+<?php include_once getComponentPath('home/bars') ?>
+
+<hr class="offset-lg">
+<hr class="offset-md">
+
+<?php
+include_once getComponentPath('home/top-desktop');
+
+include_once getComponentPath('home/top-hybrids');
+
+include_once getComponentPath('home/top-tablets');
+
+include_once getComponentPath('home/last-articles');
